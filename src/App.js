2ae627e0eb7c78d8a20a153ff4e2fdfc60928238
Brannan Conrad <brannanconrad@gmail.com>
2018-10-16 11:18:07 -0500

    Seperated components

--- src/App.js
+++ src/App.js
@@ -1,62 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import Player from './Player';
 
-function Header(props) {
-  return (
-    <header>
-      <h1>Scoreboard</h1>
-      <span className="stats">Players: {props.totalPlayers}</span>
-    </header>
-  );
-}
-
-const Player = (props) => {
-  return (
-    <div className="player">
-      <span className='player-name'>
-      <button className="remove-player" onClick={ () => props.removePlayer(props.id) }>x</button>
-    {props.name}
-      </span>
-      <Counter />
-    </div>
-  );
-}
-
-class Counter extends Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      score: 0
-    };
-  }
-
-  incrementScore () {
-    this.setState( prevState => {
-      return {
-      score: prevState.score + 1
-      };
-    })
-  }
-
-  decrementScore () {
-    this.setState( prevState => {
-      return {
-      score: this.state.score - 1
-      };
-    })
-  }
-
-  render () {
-    return (
-      <div className="counter">
-        <button className='counter-action decrement' onClick={this.decrementScore.bind(this)}> - </button>
-        <span className='counter-score'>{this.state.score}</span>
-        <button className='counter-action increment' onClick={this.incrementScore.bind(this)}> + </button>
-      </div>
-    );
-  }
-}
 
 class App extends Component {
 
@@ -64,14 +11,24 @@ class App extends Component {
     players: [
       {
         name: 'me',
+        score: 0,
         id: 1
       },
       {
         name: 'them',
+        score: 0,
         id: 2
       }
     ]
   };
+
+  handleScoreChange = (index, delta) => {
+    this.setState( prevState => {
+      return {
+      score: prevState.players[index].score += delta
+      };
+    })
+  }
 
   handleRemovePlayer = (id) => {
     this.setState( prevState => {
@@ -85,11 +42,14 @@ class App extends Component {
     return (
       <div className="scoreboard">
        <Header title='Scoreboard' totalPlayers={this.state.players.length}/>
-       {this.state.players.map( player =>
+       {this.state.players.map( (player, index) =>
         <Player 
           name={player.name}
+          score={player.score}
           key={player.id.toString()}
           id={player.id}
+          index={index}
+          changeScore={this.handleScoreChange}
           removePlayer={this.handleRemovePlayer} 
         />
        )}
